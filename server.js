b40d3f513d75116390db716b7a2053fdd227b3bc
Rohit Pandit <rpandit0907@gmail.com>
2026-07-31 22:33:19 +0530

    Deployment

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        httpOnly: true,
+        httpOnly: false,
         secure: process.env.NODE_ENV === "production",
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 24 * 60 * 60 * 1000
